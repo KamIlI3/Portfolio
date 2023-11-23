e2991c4ee3c0614fedb207f54f318d9d5c28aba0
KamIlI3 <122115129+KamIlI3@users.noreply.github.com>
2023-11-23 23:23:19 +0100

    Update

--- js/script_projekty.js
+++ js/script_projekty.js
@@ -1,12 +1,3 @@
-//Wyświetlenie diva z opisem projektu, po naciśnięciu przycisku przypisanego do projektu
-function showProject(projectId){
-    const description = document.getElementsByClassName("description");
-    for (let i=0;i<description.length; i++){
-        description[i].style.display = "none";
-    }
-    var selectedProject = document.getElementById(projectId);
-        selectedProject.style.display = 'block';
-}
 //Ustawianie stanu widoczności diva w LocalStorage
 function setVisibilityState(projectId) {
     localStorage.setItem('selectedProject', projectId);
