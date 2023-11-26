6742641e9fbb557bdf6ba068d6210e99f03d4479
KamIlI3 <122115129+KamIlI3@users.noreply.github.com>
2023-11-26 21:49:11 +0100

    Kontakt update

---
+++ js/script_kontakt.js
@@ -0,0 +1,7 @@
+function sendEmail(){
+    const myEmail = "kami3x3s@gmail.com"
+    const emial = document.getElementById("userEmail").value;
+    const contents = document.getElementById("contents").value;
+
+
+}
