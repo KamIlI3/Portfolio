cfea2f51790acff9aedd1c724b83735efd608c1c
KamIlI3 <122115129+KamIlI3@users.noreply.github.com>
2023-12-08 20:23:13 +0100

    Lightbox JavaScript comment

--- js/script_lightbox.js
+++ js/script_lightbox.js
@@ -1,21 +1,21 @@
 const lightbox = document.createElement('div');
 lightbox.id = 'lightbox';
-document.body.appendChild(lightbox);
+document.body.appendChild(lightbox); //Dołączenie diva
 
 const images = document.querySelectorAll('img');
 images.forEach(image => {
-    image.addEventListener('click', e => {
-        lightbox.classList.add('active');
-        const img = document.createElement('img');
-        img.src = image.src;
-        while (lightbox.firstChild){
-            lightbox.removeChild(lightbox.firstChild);
+    image.addEventListener('click', e => {  //Nadanie kazdemu selektorowi Image eventu click    
+        lightbox.classList.add('active'); //Przypisanie do klasy "active"
+        const img = document.createElement('img'); //Przypisanie do zminennej towrzenie elementu IMG
+        img.src = image.src; //Przypisanie źródła do zmiennej
+        while (lightbox.firstChild){ 
+            lightbox.removeChild(lightbox.firstChild); //Usunięcie zdjecia po wyjściu z lightbox
         }
-        lightbox.appendChild(img);
+        lightbox.appendChild(img); //Wyświetlenie zdjęcia w lightbox
     });
 });
 
 lightbox.addEventListener('click', e => {
     if(e.target !== e.currentTarget) return
-    lightbox.classList.remove('active');
+    lightbox.classList.remove('active'); //Wyjście z lightbox bo kliknieciu obok zdjecia
 });
